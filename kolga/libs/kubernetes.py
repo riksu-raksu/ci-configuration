681959dd22f74152e77093b7836ed01e51e14613
riku <5541148+riksu-raksu@users.noreply.github.com>
2020-09-03 09:27:17 +0300

    syntax

--- kolga/libs/kubernetes.py
+++ kolga/libs/kubernetes.py
@@ -420,7 +420,7 @@ class Kubernetes:
     def deploy_service(self, service: "Service", namespace: str, track: str) -> None:
         deploy_name = get_deploy_name(track=track, postfix=service.name)
 
-        print(f"SERVICE IS {service..__dict__}")
+        print(f"SERVICE IS {service.__dict__}")
 
         self.helm.upgrade_chart(
             chart=service.chart,
